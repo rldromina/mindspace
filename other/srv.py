e7c18d03ec212a9ad0c48431837c8cb4154f70d2
Matías Grinberg <mgrinberg@DH.digitalhouse.com>
2018-11-18 00:39:03 -0300

    ''

--- other/srv.py
+++ other/srv.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template, request
 from flask_ngrok import run_with_ngrok
 import os
 from time import strftime
+from flask_cors import CORS
 
 exp_path = os.path.dirname(__file__)
 app = Flask(__name__, template_folder=exp_path)
+
+CORS(app)
 run_with_ngrok(app)
 
 @app.route('/')
